a151e2758e5c95cd0721110e1c9cfc0b39013054
Neuber Oliveira <neuberdesigns@hotmail.com>
2015-02-13 11:06:20 -0200

    Adicionado suporte para Wordpress Adicionada permissao de execucao Adicionada a flag --laravel que indica se o arquivo é do Laravel Adicionada a flag --wordpress que indica se o arquivo é do wordpress
    Foi tudo reorganizado para facilitar a inclusao de novos bancos como postgree, sqlite e etc.

--- db-connection.py
+++ db-connection.py
@@ -1,34 +1,81 @@
+#!/usr/bin/python
+
 import subprocess as sub
 import sys
 import argparse
 import json
+import re
 
-parser = argparse.ArgumentParser(description='Connect to a laravel database using its configuration file')
+def laravelGatherInfo(path):
+	php_parse  = 'php -r "print json_encode(require \''+path+'\');"'
+
+	proc = sub.Popen(php_parse, shell=True, stdout=sub.PIPE)
+	php_json_config = proc.stdout.read()
+	json_config = json.loads( php_json_config )
+
+	default_db = json_config['default']
+	
+	connection 	= json_config['connections']['mysql']
+	host	 	= connection['host']
+	db 			= connection['database']
+	user 		= connection['username']
+	password	= connection['password']
+	
+	return {'dbname':default_db, 'host':host, 'database':db, 'user':user, 'password':password};
+
+def wordpressGatherInfo(file):
+	data = None
+	fileContents = file.read()
+	regexDbHost = "(//)?define\('DB_HOST',\s?'([a-zA-Z0-9_@./-]+)'\)"
+	regexDbName = "(//)?define\('DB_NAME',\s?'([a-zA-Z-1_@-]+)'\)"
+	regexDbUser = "(//)?define\('DB_USER',\s?'([a-zA-Z0-9_@-]+)'\)"
+	regexDbPass = "(//)?define\('DB_PASSWORD',\s?'([a-zA-Z_0-9@-]+)'\)"
+	
+	host 		= wordpressExtractVarInfo( re.findall(regexDbHost, fileContents) )
+	db 		= wordpressExtractVarInfo( re.findall(regexDbName, fileContents) )
+	user 	= wordpressExtractVarInfo( re.findall(regexDbUser, fileContents) )
+	pswd 	= wordpressExtractVarInfo( re.findall(regexDbPass, fileContents) )
+	
+	if host or db or user or pswd :
+		data = {'dbname':'mysql', 'host':host, 'database':db, 'user':user, 'password':pswd};
+	
+	return data;
+	
+def wordpressExtractVarInfo(regexResult):
+	varValue = None
+	for row in regexResult :
+		if row[0]=='' :
+			varValue = row[1]
+			
+	return varValue
+
+
+dbdata = None	
+connection_comand = None
+available = ['mysql']
+parser = argparse.ArgumentParser(description='Connect to a Laravel or Wordpress database using its configuration file')
 parser.add_argument('-f', '--file', action='store', nargs='?', type=argparse.FileType('r'), required=True, help='the configuration file')
+parser.add_argument('--laravel', action='store_true', default=True, help='if its a Laravel configuration file')
+parser.add_argument('--wordpress', action='store_true', default=False, help='if its a Wordpress configuration file')
 args = parser.parse_args()
 
-# if the script don't need output.
-#sub.call("php args.file.name")
 
-# if you want output
-php_parse  = 'php -r "print json_encode(require \''+args.file.name+'\');"'
+if args.wordpress :
+	dbdata = wordpressGatherInfo( args.file )
+elif args.laravel :
+	dbdata = laravelGatherInfo( args.file.name )
 
-proc = sub.Popen(php_parse, shell=True, stdout=sub.PIPE)
-php_json_config = proc.stdout.read()
-json_config = json.loads( php_json_config )
 
-default_db = json_config['default']
-
-if default_db != 'mysql' :
-	print 'Default is not a MySQL database'
+if dbdata == None :
+	print 'An error ocurred: Could not define database info'
 	sys.exit()
 
-mysql_node = json_config['connections']['mysql']
-host	 	= mysql_node['host']
-db 			= mysql_node['database']
-user 		= mysql_node['username']
-password	= mysql_node['password']
+if not (dbdata['dbname'] in available) :
+	print dbdata['dbname']+' is not supported yet'
+	sys.exit()
 
-mysql_cmd = ['mysql', '-h'+host, '-u'+user, '-p'+password, db]
+if dbdata['dbname']=='mysql' :
+	connection_comand = ['mysql', '-h'+dbdata['host'], '-u'+dbdata['user'], '-p'+dbdata['password'], dbdata['database']]
 
-sub.call(mysql_cmd)
+if not connection_comand==None :
+	sub.call(connection_comand)
